4a2704799a47141e5eeffbe83240b972bc7feaa5
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-04-28 23:59:54 -0300

    feat: add token by regex

--- simple-scanning/Token.py
+++ simple-scanning/Token.py
@@ -3,11 +3,20 @@ from TokenType import TokenType
 
 class Token:
 
-    def __init__(self, token_type: TokenType, token_value: str):
+    def __init__(self, token_value: str):
 
-        self.type = token_type
+        self.type = self.tokenTypeByRegex(token_value)
         self.lexeme = token_value
 
     def __str__(self):
-
         return f"Token [type={self.type.name}, lexeme={self.lexeme}]"
+
+    def tokenTypeByRegex(self, token_value: str):
+        number_regex = r'^\d+$'
+        operator_regex = r'^[+\-*/]$'
+        if re.match(number_regex, token_value):
+            return TokenType.INT
+        elif re.match(operator_regex, token_value):
+            return TokenType.OPERATOR
+        else:
+            raise ValueError(f'Invalid token: {token_value}')
